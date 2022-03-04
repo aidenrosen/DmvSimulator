629a970c7e1492b70d750a7b846493eab6551a51
aidenrosen <aidenrrosen@gmail.com>
2022-03-03 21:42:23 -0500

    Added a miracle.  The user has their papers.

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -12,7 +12,8 @@ public class DmvSimulator
 			System.out.println("Next up: " +  i);
 		}
 		System.out.println("Next up: " + num);
-		System.out.println("You don't have the required paperwork! Sort that out before coming here and wasting people's time!  NEXT!!!");
+		if((int) Math.random() * 100 == 1) System.out.println("I've tried to find something that you're missing, but I think you have everything.  You're all set");
+		else System.out.println("You don't have the required paperwork! Sort that out before coming here and wasting people's time!  NEXT!!!");
 	}
 
 }
